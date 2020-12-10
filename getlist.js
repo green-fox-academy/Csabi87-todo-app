e4b98bc9b47efa468f8722a28a1f0db24a973358
Csabi <szabo.csaba87@live.com>
2020-12-10 18:09:20 +0100

    add new statement to getList func in getlist.js

--- getlist.js
+++ getlist.js
@@ -2,6 +2,9 @@ import fs from 'fs';
 
 function getList() {
     let fileContent = fs.readFileSync('./list.txt').toString();
+    if (fileContent.length === 0) {
+        console.log('Nincs mára tennivalód! :)')
+    }
     console.log(fileContent)
 }
 
