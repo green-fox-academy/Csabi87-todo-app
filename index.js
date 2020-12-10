6170357ef1e650458de6f5b760ff58210f087cad
Csabi <szabo.csaba87@live.com>
2020-12-10 16:49:29 +0100

    install fs

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ const args = minimist(process.argv);
 if (Object.keys(args).length === 1) {
     printUserManual();
 } else if (args._.length > 2 || !Object.keys(args).every(arg => ['_', 'l', 'a', 'r', 'c'].includes(arg))) {
-    // throw Error('invalid option');
     printUserManual();
 }
